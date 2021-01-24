77716382945799f5e56a3bb225a9488a71a31341
philipiaeveline <philipiaeveline13@gmail.com>
2021-01-24 08:03:50 +0300

    add

--- awward2/urls.py
+++ awward2/urls.py
@@ -4,15 +4,12 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns=[
-   url(r'^$',views.news_today,name='newsToday'),
-   url(r'^archives/(\d{4}-\d{2}-\d{2})/$',views.past_days_news,name = 'pastNews'),
-   url(r'^search/', views.search_results, name='search_results'),
-   url(r'^article/(\d+)',views.article,name ='article'),
-   url(r'^new/article$', views.new_article, name='new-article'),
-   url(r'^ajax/newsletter/$', views.newsletter, name='newsletter'),
-   url(r'^api/merch/$', views.MerchList.as_view())
-
-
+   path('',views.index,name='index'),
+   path('new/profile',views.new_profile, name='profile'),
+   path('search/', views.search_results, name='search_results'),
+   path('logout', views.logout, name='logout'),
+   path('project/', views.project,name='project'),
+   path('rating/<int:pk/', views.rating,name='rating'),
   
 ]
 
